8dac146ae960983567ee42aaad1572ca4710c4eb
AlexBoyd <alexander.ray.boyd@gmail.com>
2015-06-19 13:57:51 -0700

    adding in text to ar windshield.

---
+++ IAT445/Assets/ARWindShield.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class ARWindShield : MonoBehaviour
+{
+
+	public Text ARText;
+
+	[TextArea(3, 10)]
+	public string
+		ArtificialGravityOK;
+	[TextArea(3, 10)]
+	public string
+		EnginesOK;
+	[TextArea(3, 10)]
+	public string
+		PowerSystemsOK;
+	[TextArea(3, 10)]
+	public string
+		LifeSupportOK;
+	[TextArea(3, 10)]
+	public string
+		LifeSupportError;
+
+	public void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			ARText.text = ArtificialGravityOK;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			ARText.text = EnginesOK;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			ARText.text = PowerSystemsOK;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			ARText.text = LifeSupportOK;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha5)) {
+			ARText.text = LifeSupportError;
+		}
+	}
+
+}
