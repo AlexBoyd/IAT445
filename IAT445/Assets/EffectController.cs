a22b077f0d7240fb5825ac5572b0c3c65bd7bd44
AlexBoyd <alexander.ray.boyd@gmail.com>
2015-06-20 20:18:09 -0700

    more audio and stuff

---
+++ IAT445/Assets/EffectController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectController : MonoBehaviour
+{
+
+	public GameObject _HyperDriveAudio;
+	public GameObject _HyperDriveAnimation;
+	public GameObject _AlarmAudio;
+	public GameObject _AlarmAnimation;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.A)) {
+			_AlarmAudio.SetActive (!_AlarmAudio.activeInHierarchy);
+			_AlarmAnimation.SetActive (!_AlarmAnimation.activeInHierarchy);
+		}
+
+		if (Input.GetKeyDown (KeyCode.H)) {
+			_HyperDriveAudio.SetActive (!_HyperDriveAudio.activeInHierarchy);
+			//_AlarmAnimation.SetActive (!_AlarmAnimation.activeInHierarchy);
+		}
+	}
+}
