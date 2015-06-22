a2b27eda032c8038c619aad35141d9869892280f
AlexBoyd <alexander.ray.boyd@gmail.com>
2015-06-21 21:06:53 -0700

    more hyperdrive stuff audio spin

---
+++ IAT445/Assets/PinToCapsule.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinToCapsule : MonoBehaviour
+{
+
+	public Transform TargetTransform;
+
+	public void Update ()
+	{
+		transform.localRotation = TargetTransform.rotation;
+		transform.position = transform.localRotation * new Vector3 (TargetTransform.localScale.x, TargetTransform.localScale.y, TargetTransform.localScale.x) / 2;
+	}
+}
---
+++ IAT445/Assets/toggleMouseLook.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class toggleMouseLook : MonoBehaviour
+{
+
+
+	public MouseLook mouseLook;
+	// Use this for initialization
+	void Start ()
+	{
+		mouseLook = GetComponent<MouseLook> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyUp (KeyCode.M)) {
+			mouseLook.enabled = !mouseLook.enabled;
+		}
+	}
+}
