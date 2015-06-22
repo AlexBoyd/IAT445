4d9f42a47ce593b236b291b18954efccd255c96d
AlexBoyd <alexander.ray.boyd@gmail.com>
2015-06-21 21:43:24 -0700

    hyperdrive lighting effects

---
+++ IAT445/Assets/TransparencyToIntensity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransparencyToIntensity : MonoBehaviour
+{
+
+
+	public Light DestionationLight;
+	public Renderer SourceRender;
+	public float Scale;
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		DestionationLight.intensity = SourceRender.material.GetFloat ("_Transparency") * Scale;
+	}
+}
