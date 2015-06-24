24e3b5d8197e39d815d2239a0962151a99def666
AlexBoyd <alexander.ray.boyd@gmail.com>
2015-06-24 11:20:25 -0700

    light mapping technology!. Also more animations tweaks

---
+++ IAT445/Assets/lightIntensityMapper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightIntensityMapper : MonoBehaviour
+{
+
+	public Light _sourceLight;
+
+	private Material _mat;
+
+	void Start ()
+	{
+		_mat = GetComponent<Renderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_mat.SetColor ("_EmissionColor", _sourceLight.color * _sourceLight.intensity * 0.1f);
+	}
+}
